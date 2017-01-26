53f670965344de1dd57d59342a5672a6e79f865a
Petar Petrov <2202331P@student.gla.ac.uk>
2017-01-26 13:42:50 +0000

    Chapter 6 w/o last exercise

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	category_list=Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories':category_list}
 	return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
 	context_dict = {'author': "Petar Petrov"}
 	return render(request,'rango/about.html',context=context_dict)
+	
+def show_category(request, category_name_slug):
+	context_dict={}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+	return render(request,'rango/category.html',context_dict)
